b5b56d3653c65b5a0944189493db61cf0c31bd0a
René Oelke <rene.oelke@gmail.com>
2012-08-17 01:08:41 +0200

    Changed database schema

--- scripts/schema.twitterwall.sql
+++ scripts/schema.twitterwall.sql
@@ -1,21 +1,22 @@
 -- scripts/schema.twitterwall.sql
 
 --
--- Table `tweet` 
+-- Table `tweet`
 --
 CREATE TABLE
 	`tweet`
 (
-    `id` BIGINT NOT NULL PRIMARY KEY,
-	-- `idStr` VARCHAR(255) NOT NULL,
-	-- `fromUser` VARCHAR(255) NOT NULL,
-	-- `fromUserIdStr` VARCHAR(255) NOT NULL,
-	-- `fromUserName` VARCHAR(255) NOT NULL,
+	`id` BIGINT NOT NULL PRIMARY KEY,
+	`idStr` VARCHAR(255) NOT NULL,
+	`query` VARCHAR(255) NOT NULL,
+	`fromUser` VARCHAR(255) NOT NULL,
+	`fromUserIdStr` VARCHAR(255) NOT NULL,
+	`fromUserName` VARCHAR(255) NOT NULL,
 	-- `isoLanguageCode` VARCHAR(2) NOT NULL,
 	-- `profileImageUrl` TEXT NOT NULL,
 	-- `profileImageUrlHttps` TEXT NOT NULL,
-	-- `source` TEXT NOT NULL,
-	-- `text`TEXT NOT NULL,
+	`source` TEXT NOT NULL,
+	`text`TEXT NOT NULL,
 	-- `toUser` VARCHAR(255) NULL,
 	-- `toUserIdStr` VARCHAR(255) NULL,
 	-- `toUserName` VARCHAR(255) NULL,
@@ -26,7 +27,7 @@ CREATE TABLE
 
 --
 -- Index `id`
--- 
+--
 CREATE INDEX
 	`id`
 ON
