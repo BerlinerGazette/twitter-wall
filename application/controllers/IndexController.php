e232b39d82ef9a81ab419e05086cad3c70378498
Marcel Eichner <love@ephigenia.de>
2012-09-20 12:16:01 +0200

    JSON Output
    * Added json output rendering instead of HTML rendering
    * json output triggered by Request ?ajax=1, "Content-Type: application/json" or by ajax calls from jQuery

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -16,19 +16,46 @@ class IndexController extends Zend_Controller_Action
             'pp' => 10,
             'q' => null,
         );
+        $Request = $this->getRequest();
         $query = $this->getRequest()->getQuery();
         $config = array_merge($defaults, $query);
         // pagination
         $page = $config['p'];
         $perPage = $config['pp'];
-        $query = $config['q'];
+        $searchQuery = $config['q'];
         // get the tweets
         $Table = new Application_Model_DbTable_Tweet(array(
             'rowClass' => 'Application_Model_Tweet'
         ));
-        $tweets = $Table->search($query, ($page - 1) * $perPage, $perPage);
+        $tweets = $Table->search($searchQuery, ($page - 1) * $perPage, $perPage);
+
+        // send json bodies or render HTML?
+        $isXmlHttpRequest = $Request->isXmlHttpRequest();
+        $isJSONContentType = $Request->getHeader('Content-Type') == 'application/json';
+        $isGetAjax = isset($query['ajax']);
+        if ($isGetAjax || $isJSONContentType || $isXmlHttpRequest) {
+            $data = array();
+            foreach($tweets as $Tweet) {
+                $date = new DateTime($Tweet->getCreatedAt());
+                $tweetData = array(
+                    'id' => $Tweet->getIdStr(),
+                    'user' => array(
+                        'name' => $Tweet->getFromUser(),
+                        'id' => $Tweet->getFromUserIdStr(),
+                        'username' => $Tweet->getFromUserName(),
+                    ),
+                    'source' => $Tweet->getSource(),
+                    'text' => $Tweet->getText(),
+                    'created' => $date->format('c'),
+                );
+                $data[] = $tweetData;
+            }
+            $this->getHelper('json')->sendJson($data);
+            exit;
+        }
+
         // assign for the view
         $this->view->assign('tweets', $tweets);
-        $this->view->assign('q', $query);
+        $this->view->assign('q', $searchQuery);
     }
 }
